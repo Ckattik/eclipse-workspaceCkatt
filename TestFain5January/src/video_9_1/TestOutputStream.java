84ceae6f2833caa4f29fdb1fdaa5f58a1d554f57
Ckatt <0932814148_ckatt@ukr.net>
2018-02-08 22:36:11 +0200

    Video_9_1 TestOutputStream

---
+++ TestFain5January/src/video_9_1/TestOutputStream.java
@@ -0,0 +1,43 @@
+package video_9_1;
+
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+public class TestOutputStream {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+ 
+		int somedata [] = { 34, 56, 76, 234, 324, 675};
+		
+		FileOutputStream myFile = null;
+		
+		try {
+			
+			myFile = new FileOutputStream("C:\\Users\\Ckatt\\eclipse-workspace\\TestFain5January\\src\\video_9_1\\xyz");
+			for(int i = 0; i < somedata.length; i++) {
+				myFile.write(somedata[i]);
+			}
+			
+			
+		}catch(IOException e) {
+			System.out.println("Could not write to a file: " + e.toString());
+			
+		}finally {
+			if(myFile != null) {
+				try {
+					myFile.close();
+				}catch(Exception e1) {
+					e1.printStackTrace();
+				}
+			}
+		}
+		
+		
+		
+		
+		
+		
+	}
+
+}
