49aedc2671a2b5db030ae224aaae496921f74a72
Ckattik <Ckatt@ukr.net>
2018-03-03 06:29:38 +0200

    added Order

---
+++ TestFain5January/bin/video_13_0/Order.java
@@ -0,0 +1,22 @@
+package video_13_0;
+
+public class Order {
+
+	public int oderID;
+	public String stockSymbol;
+	public int quantity;
+	public float price;
+	
+	
+	
+	public Order(int ID, String stockSymbol, int quantity, float price) {
+		
+		this.oderID = ID;
+		this.stockSymbol = stockSymbol;
+		this.quantity = quantity;
+		this.price = price;
+		
+		}
+	
+	
+}
